2456a797d51eeee4698571cf0b8c4e8c51b5e11f
Grant Ferment <grantferment@gmail.com>
2020-11-06 18:24:22 -0500

    working quiz

--- Assets/game.js
+++ Assets/game.js
@@ -2,13 +2,13 @@
 
 var questions = [
     {
-        question: "How would you type the (beginning) script tag?",
+        question: "How would you link JavaScript page to an HTML?",
         choices: [
             "<script type=___.js> ",
             "<script url=____.js> ",
             "<script src=____.js> ",
             "<script href=____.js> "
-            
+
         ],
         answerIndex: 2
     },
@@ -41,21 +41,21 @@ var questions = [
             "Function"
         ],
         answerIndex: 1
-    }, 
+    },
 ]
 
 //Other variables
-var currentQuestion = document.querySelector("questionText");
-var starterBtn = document.querySelector("startBtn");
-var timerEl = document.querySelector("timer");
-var message = document.querySelector("message");
-var startBox = document.getElementById("startpage");
-var questionBox = document.getElementById("questionBox")
-var timerSpan = document.getElementById("timerSpan")
-var choiceA = document.getElementById("choiceA")
-var choiceB = document.getElementById("choiceB")
-var choiceC = document.getElementById("choiceC")
-var choiceD = document.getElementById("choiceD")
+var questionText = document.querySelector("#questionText");
+var starterBtn = document.querySelector("#startBtn");
+var timerEl = document.querySelector("#timer");
+var message = document.querySelector("#message");
+var startBox = document.querySelector("#startpage");
+var questionBox = document.querySelector("#questionBox")
+var timerSpan = document.querySelector("#timerSpan")
+var choiceA = document.querySelector("#choiceA")
+var choiceB = document.querySelector("#choiceB")
+var choiceC = document.querySelector("#choiceC")
+var choiceD = document.querySelector("#choiceD")
 var secondsLeft = 60
 
 
@@ -64,31 +64,32 @@ var currentQuestionIndex = 0
 
 
 function displayQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
-    question.innerHTML = [currentQuestion].question
 
-    for (var i = 0; i < currentQuestion.choices.length; i++) {
-       var choice = document.createElement("li");
-       choice.innerHTML = questions[i].question;
-       choice.setAttribute("id", i);
-       questions.appendChild(choice);
-    }
-  
+    questionText.textContent = questions[currentQuestionIndex].question;
+
+    choiceA.textContent = questions[currentQuestionIndex].choices[0]
+    choiceB.textContent = questions[currentQuestionIndex].choices[1]
+    choiceC.textContent = questions[currentQuestionIndex].choices[2]
+    choiceD.textContent = questions[currentQuestionIndex].choices[3]
 
 }
 
 
-//answer is chosen, need to map to when clicking a button this will run
-function answerChosen() {
+//answer is chosen, this will run whenever an answer is chosen using onClick for each button
+function answerChosen(answer) {
+    // if question is wrong, subtracts 10 seconds
+    if (answer != questions[currentQuestionIndex].answerIndex) {
+        secondsLeft -= 10;
+    }
+    // currentQuestionIndex increases by 1, which will change to the next question
     currentQuestionIndex++;
-    displayQuestion();
+    // When currentQuestionIndex exceeds the number of quesitons, the game will end
+    if (currentQuestionIndex > 3) {
+        localStorage.setItem("Score", secondsLeft);
+        window.location.href = "highscore.HTML";
+    }
 
-    //if ( === answerIndex) {
-   //     message.textContent("Correct")
-   // }
-    //else {
-   //     message.textContent("Wrong")
-    //}    
+    displayQuestion();
 }
 
 
@@ -98,23 +99,22 @@ function startQuiz() {
     questionBox.style.display = "block";
     displayQuestion();
     setTime();
-    }
-    
+}
+
 // timer
 function setTime() {
-        var timerInterval = setInterval(function(){
-            secondsLeft--;
-            timerSpan.textContent = secondsLeft;
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerSpan.textContent = secondsLeft;
+        //if timer reaches 0, game ends and sends score to the highscore HTML
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            localStorage.setItem("Score", secondsLeft);
+            window.location.href = "highscore.HTML";
+        }
+    }, 1000);
+}
 
-            if(secondsLeft === 0) {
-                clearInterval(timerInterval);
-                //quizEnd();
-            // need to make it so it will go to end screen with score = 0                
-            }
-        }, 1000);
-    }
-
-//function quizEnd();
 
 
 
